d3a640f2d880ec96296a469d97ca38791d8ae006
Sy Rashid <sy.rashid.11@gmail.com>
2020-07-08 22:46:41 +1000

    Set up html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: ['./src/js/index.js'],
@@ -9,4 +10,10 @@ module.exports = {
   devServer: {
     contentBase: './dist'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ],
 };
