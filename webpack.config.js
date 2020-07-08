be1882573668c09e0d244202df2442d0e6a2951b
Sy Rashid <sy.rashid.11@gmail.com>
2020-07-09 00:23:42 +1000

    FEAT: Able to pack scss files, need to compress/minify and separte from js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: ["./src/js/index.js"],
@@ -14,7 +15,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./src/index.html",
-    }),
+    })
   ],
   module: {
     rules: [
@@ -29,6 +30,6 @@ module.exports = {
         test: /\.(s*)css$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       }
-    ],
-  },
+    ]
+  }
 };
