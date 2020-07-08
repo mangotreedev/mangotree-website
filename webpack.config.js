ae018bfa0b04b166676938ce2bf6c5b22e3fcd6e
Sy Rashid <sy.rashid.11@gmail.com>
2020-07-08 22:35:11 +1000

    ARCH: Added webpack cli

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: ['./src/js/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  devServer: {
+    contentBase: './dist'
+  },
+};
