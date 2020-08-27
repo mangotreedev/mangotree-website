d4f0e8b3e6fa5915d169e9846f4a0b76f35eb558
Nico Proto <nicolasproto@gmail.com>
2020-08-27 18:21:51 +1000

    FEAT: Finished navbar with links

--- src/js/components/dist/init_navbar.dev.js
+++ src/js/components/dist/init_navbar.dev.js
@@ -6,10 +6,22 @@ Object.defineProperty(exports, "__esModule", {
 exports.initNavbar = void 0;
 
 var initNavbar = function initNavbar() {
+  // TODO: Disable scrolling when navbar is open
   var navbarIcon = document.querySelector('.js-navbar-icon');
   navbarIcon.addEventListener('click', function (event) {
     event.currentTarget.classList.toggle('burger-icon--active');
+    document.querySelector('.navbar-menu').classList.toggle('navbar-menu--active');
   });
+  var nabarLinks = document.querySelectorAll('.js-links-list li');
+
+  var closeNavbar = function closeNavbar(link) {
+    link.addEventListener('click', function () {
+      document.querySelector('.navbar-menu').classList.remove('navbar-menu--active');
+      navbarIcon.classList.toggle('burger-icon--active');
+    });
+  };
+
+  nabarLinks.forEach(closeNavbar);
 };
 
 exports.initNavbar = initNavbar;
--- src/js/index.js
+++ src/js/index.js
@@ -3,14 +3,11 @@ import "../css/website.scss";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import * as testComp from './components/test_component';
-
 import { initDefineBtn } from "./components/init_define_btn";
 import { initSoundIcons } from "./components/init_sound_icons";
 import { initFadeEffect } from "./components/init_fade_effect";
 import { initNavbar } from "./components/init_navbar";
 
-testComp.testFunction();
 initDefineBtn();
 initSoundIcons();
 initFadeEffect();
