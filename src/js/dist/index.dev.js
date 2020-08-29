3f8fde3d130070e2c052905f2a128e62317b7545
Nico Proto <nicolasproto@gmail.com>
2020-08-29 20:59:30 +1000

    FEAT: Added js to scroll particular elements to the left on load

--- src/js/dist/index.dev.js
+++ src/js/dist/index.dev.js
@@ -14,7 +14,16 @@ var _init_fade_effect = require("./components/init_fade_effect");
 
 var _init_navbar = require("./components/init_navbar");
 
+var _init_schedule_btn = require("./components/init_schedule_btn");
+
+var _init_drawing_trigger = require("./components/init_drawing_trigger");
+
+var _init_left_scroll = require("./components/init_left_scroll");
+
+(0, _init_schedule_btn.initScheduleBtn)();
+(0, _init_drawing_trigger.initDrawingTrigger)();
 (0, _init_define_btn.initDefineBtn)();
 (0, _init_sound_icons.initSoundIcons)();
 (0, _init_fade_effect.initFadeEffect)();
 (0, _init_navbar.initNavbar)();
+(0, _init_left_scroll.initLeftScroll)();
