23b130f358944405646bf44fb59dcd55dfc68e5e
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-08 16:10:43 +1000

    FIX: Removed triggers

--- src/js/components/init_portfolio_parallax.js
+++ src/js/components/init_portfolio_parallax.js
@@ -9,8 +9,8 @@ export const initPortfolioParallax = (controller) => {
 
     const tl = gsap.timeline();
     tl.to(child, 1, { rotation: -3}, 0);
-    tl.to(child, 1, { y: 100, ease: Linear.easeNone }, 0);
-    tl.to(background, 1, { y: 50, ease: Linear.easeNone }, 0);
+    tl.to(child, 1, { y: 80, ease: Linear.easeNone }, 0);
+    tl.to(background, 1, { y: 40, ease: Linear.easeNone }, 0);
 
     const scene = new ScrollMagic.Scene({
       triggerElement: card,
@@ -18,14 +18,7 @@ export const initPortfolioParallax = (controller) => {
       duration: "100%"
     });
 
-    scene.setTween(tl)
-      .addIndicators({
-        colorTrigger: "black",
-        colorStart: "black",
-        colorEnd: "black",
-        indent: 10
-      })
-      .addTo(controller);
+    scene.setTween(tl).addTo(controller);
   }
 
 }
