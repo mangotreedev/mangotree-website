2886cd7420c51bb42dd38792a49c71c7b8085da3
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-08 14:46:57 +1000

    FIX: Parallax styling fixes with background

--- src/js/components/init_portfolio_parallax.js
+++ src/js/components/init_portfolio_parallax.js
@@ -1,13 +1,17 @@
 export const initPortfolioParallax = (controller) => {
   var tl = gsap.timeline();
-  var child = document.querySelector('.portfolio-card-display__image');
 
-  tl.to(child, 1, { y: -180, ease: Linear.easeNone });
+
+  var child = document.querySelector('.portfolio-card-display__image');
+  var background = document.querySelector('.portfolio-card-display__background');
+
+  tl.to(child, 1, { y: -120, ease: Linear.easeNone }, 0);
+  tl.to(background, 1, { y: -60, ease: Linear.easeNone }, 0);
 
   var scene = new ScrollMagic.Scene({
-    triggerElement: document.querySelector('.portfolio-card-display'),
-    triggerHook: 0.4,
-    duration: "100%"
+    triggerElement: document.querySelector('.portfolio-card'),
+    triggerHook: 0.8,
+    duration: "80%"
   })
   scene.setTween(tl)
     .addIndicators({
