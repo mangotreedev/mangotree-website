514d73c8ea955acd64d9a014fd59cefb67e4f091
Sy Rashid <sy.rashid.11@gmail.com>
2020-09-14 14:54:54 +0300

    FIX: Fixed transition two to three

--- src/js/components/init_team_transitions.js
+++ src/js/components/init_team_transitions.js
@@ -29,7 +29,7 @@ const oneToTwo = () => {
     tl.set(('.team-transition--part-one'), { css: { zIndex: 0 } }).set(('.team-transition--part-two'), { css: { zIndex: 10 } });
 
     // Show the second button
-    tl.to('.js-team-transition__btn--part-two', 0.5, { opacity: 1 });
+    tl.to('.js-team-transition__btn--part-two', 0.4, { opacity: 1 });
 
     // Revert imgs back to proper location after movement
     tl.eventCallback("onComplete", function () {
@@ -62,23 +62,23 @@ const twoToThree = () => {
     // Build GSAP Timeline
     const tl = gsap.timeline();
     // Make text dissappear
-    tl.to('.team-transition_nico--part-two p', 0.8, { opacity: 0 }, 0);
-    tl.to('.team-transition_sy--part-two div', 0.8, { opacity: 0 }, 0);
+    tl.to('.team-transition_nico--part-two p', 0.4, { opacity: 0 }, 0);
+    tl.to('.team-transition_sy--part-two div', 0.4, { opacity: 0 }, 0);
 
     // Move photos across page, make button disappear
-    tl.to(nicoTwo, 2, { x: nicoCoords.x, y: nicoCoords.y, ease: "back.inOut(1.7)" }, 1);
-    tl.to(syTwo, 2, { x: syCoords.x, y: syCoords.y, ease: "back.inOut(1.7)" }, 1);
+    tl.to(nicoTwo, 1.5, { x: nicoCoords.x, y: nicoCoords.y, ease: "back.inOut(1.7)" }, 1);
+    tl.to(syTwo, 1.5, { x: syCoords.x, y: syCoords.y, ease: "back.inOut(1.7)" }, 1);
     tl.to(this, 0.8, { opacity: 0 }, 1);
 
     // Shake photos, and show part three
-    tl.to(nicoTwo, 0.15, { x: nicoCoords.x + 20, repeat: 5, yoyo: true, ease: Sine.easeInOut }).to(nicoTwo, 0.3, { opacity: 0 }).to(nicoThreeFull, 0.7, { opacity: 1 });
-    tl.to(syTwo, 0.15, { x: syCoords.x + 20, repeat: 5, yoyo: true, ease: Sine.easeInOut }).to(syTwo, 0.3, { opacity: 0 }).to(syThreeFull, 0.7, { opacity: 1 });
+    tl.to(nicoTwo, 0.15, { x: nicoCoords.x + 20, repeat: 2, yoyo: true, ease: Sine.easeInOut }).to(nicoTwo, 0.3, { opacity: 0 }).to(nicoThreeFull, 0.7, { opacity: 1 });
+    tl.to(syTwo, 0.15, { x: syCoords.x + 20, repeat: 2, yoyo: true, ease: Sine.easeInOut }).to(syTwo, 0.3, { opacity: 0 }).to(syThreeFull, 0.7, { opacity: 1 });
 
     // Flip the z-index for the team transitions
     tl.set(('.team-transition--part-two'), { css: { zIndex: 0 } }).set(('.team-transition--part-three'), { css: { zIndex: 10 } });
 
     // Show the third button
-    tl.to('.js-team-transition__btn--part-three', 0.5, { opacity: 1 });
+    tl.to('.js-team-transition__btn--part-three', 0.4, { opacity: 1 });
 
     // Revert imgs back to proper location after movement, and revert opacitys properly
     tl.eventCallback("onComplete", function () {
